a82d4fa4dcab1f701d0534b3b88e36a61bc46f0f
Cristian <c.rivera.henriquez@gmail.com>
2021-02-16 13:36:22 -0300

    modificacion

--- mail.php
+++ mail.php
@@ -24,8 +24,8 @@ try {
     $mail->isSMTP();                                            // Send using SMTP
     $mail->Host       = 'smtp.live.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'matador_16_11@hotmail.com';                     // SMTP username
-    $mail->Password   = 'Cristian30.';                               // SMTP password
+    $mail->Username   = '';                     // SMTP username
+    $mail->Password   = '';                               // SMTP password
     $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
     $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
