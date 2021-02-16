85dd2241b73c3a236df964f2706361b24b39d6a8
Cristian <c.rivera.henriquez@gmail.com>
2021-02-16 12:36:12 -0300

    cambios

--- mail.php
+++ mail.php
@@ -1,5 +1,12 @@
 <?php
 
+$nombre = $_POST['nombre'];
+$mensaje = $_POST['mensaje'];
+$email = $_POST['email'];
+$telefono = $_POST['telefono'];
+
+$body = "Nombre:" . $nombre . "<br>Correo: " . $email . "<br>Teléfono: " . $telefono . "<br>Mensaje: " . $mensaje;
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -9,25 +16,22 @@ require 'assets/src/SMTP.php';
 
 // Instantiation and passing `true` enables exceptions
 $mail = new PHPMailer(true);
-$nombre = $_POST['nombre'];
-$mensaje = $_POST['mensaje'];
-$email = $_POST['email'];
-$telefono = $_POST['telefono'];
+
 
 try {
     //Server settings
     $mail->SMTPDebug = 0;                      // Enable verbose debug output
     $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->Host       = 'smtp.live.com';                    // Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = '';                     // SMTP username
-    $mail->Password   = '';                               // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Username   = 'matador_16_11@hotmail.com';                     // SMTP username
+    $mail->Password   = 'Cristian30.';                               // SMTP password
+    $mail->SMTPSecure = 'ssl';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 465;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     //Recipients
-    $mail->setFrom('c.rivera.henriquez@gmail.com', 'Mailer');
-    $mail->addAddress($_POST['email'], $_POST['nombre']);     // Add a recipient
+    $mail->setFrom($email, $nombre);
+    $mail->addAddress('cristian.rivera@meetcard.cl');     // Add a recipient
 
 
  
@@ -35,7 +39,7 @@ try {
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Here is the subject';
-    $mail->Body = 'Mensaje:'.$mensaje;
+    $mail->Body = $body;
 
 
     $mail->send();
